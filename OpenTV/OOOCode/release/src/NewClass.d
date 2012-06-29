cdc9f4d46274e42099b5d3adaaa73aba8a840d08
Peter Halliday <pghalliday@gmail.com>
2012-06-29 17:40:19 +0200

    removing the build directories

--- OpenTV/OOOCode/release/src/NewClass.d
+++
@@ -1,7 +0,0 @@
-src/NewClass.d: ../src/NewClass.c ../src/NewClass.h ../src/OOOCode/OOOCode.h \
-  ../src/OOOCode/Utilities/OOOPastingAndQuoting.h \
-  ../src/OOOCode/Utilities/OOOEmptyArguments.h \
-  ../src/OOOCode/OOOCode_Interface.h ../src/OOOCode/OOOCode_ClassHeader.h \
-  ../src/OOOCode/OOOCode_ClassImplementation.h \
-  C:/OpenTV/SDKSuiteC2.2/SDK/include/opentv.h \
-  C:/OpenTV/SDKSuiteC2.2/SDK/include/assert.h ../src/NewInterface.h
