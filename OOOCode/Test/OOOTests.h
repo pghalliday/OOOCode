0e7ccb4b42206a8f3bb6892370374a6bfa017eb2
Peter Halliday <pghalliday@gmail.com>
2012-11-05 15:49:37 +0100

    restructuring

--- OOOCode/Test/OOOTests.h
+++ OOOCode/Test/OOOTests.h
@@ -1,6 +1,3 @@
-#include "OOODebugReporter.Test.h"
-#include "OOOUnitTests.Test.h"
-#include "OOOMockDebug.Test.h"
 #include "OOOEmptyArguments.Test.h"
 #include "OOOPastingAndQuoting.Test.h"
 #include "OOOCode.Test.h"
