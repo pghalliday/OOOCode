038b61f49c62145b4c7af355d192d8d298ec3c75
Peter Halliday <pghalliday@gmail.com>
2012-11-05 16:59:28 +0100

    completing the circle

--- OOOCode/Test/Main.c
+++ OOOCode/Test/Main.c
@@ -1,17 +1,17 @@
 #include "opentv.h"
 #include "assert.h"
-#include "OOODebug.h"
+#include "OOODebugLog.h"
 #include "OOODebugReporter.h"
 #include "OOOUnitTestsRun.h"
 
 void main(void)
 {
 	size_t uMemory = O_heap_available();
-	OOODebug * pDebug = OOOConstruct(OOODebug);
-	OOODebugReporter * pReporter = OOOConstruct(OOODebugReporter, OOOCast(OOOIDebug, pDebug));
+	OOODebugLog * pDebugLog = OOOConstruct(OOODebugLog);
+	OOODebugReporter * pReporter = OOOConstruct(OOODebugReporter, OOOCast(OOOILog, pDebugLog));
 	OOOUnitTestsRun(OOOCast(OOOIReporter, pReporter));
 	OOODestroy(pReporter);
-	OOODestroy(pDebug);
+	OOODestroy(pDebugLog);
 	assert(O_heap_available() == uMemory);
 
 	/* Stick around so the VSTB does not exit and we know we ran everything */
