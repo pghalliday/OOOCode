6a9ba3696174d78c405e8994f848e20898f463d8
Peter Halliday <pghalliday@gmail.com>
2012-12-03 23:47:30 +0100

    factoring out macros

--- OOOCode/Test/OOOTests.h
+++ OOOCode/Test/OOOTests.h
@@ -1,6 +1,4 @@
 #include "OOO_vprintf.Test.h"
-#include "OOOEmptyArguments.Test.h"
-#include "OOOPastingAndQuoting.Test.h"
 #include "OOOCode.Test.h"
 #include "OOOCodePublic.Test.h"
 #include "OOOCodePrivate.Test.h"
