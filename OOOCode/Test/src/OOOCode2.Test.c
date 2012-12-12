3c1bd5093226223dcbe244c498a831964f1c4a4c
Peter Halliday <pghalliday@gmail.com>
2012-12-12 18:23:45 +0100

    started class declare

--- OOOCode/Test/src/OOOCode2.Test.c
+++ OOOCode/Test/src/OOOCode2.Test.c
@@ -1,101 +1,59 @@
 #include "OOOUnitTestDefines.h"
 
 #include "OOOCode_Interface2.h"
-#include "OOOCode_ClassHeader2.h"
+//#include "OOOCode_ClassHeader2.h"
+#include "OOOPaste.h"
+#include "OOOFilter.h"
+#include "OOOPre.h"
+#include "OOOForEachList.h"
+#include "OOOIsEmpty.h"
 
-#define OOODeclareStruct(CLASS_NAME) \
-	typedef struct OOOPaste(_, CLASS_NAME) CLASS_NAME;
-#define OOODeclareConstructorType0(CLASS_NAME, ARGS...) \
-	typedef CLASS_NAME * (* OOOPaste(CLASS_NAME, _constructor))(ARGS);
-#define OOODeclareConstructorType1(CLASS_NAME) \
-	typedef CLASS_NAME * (* OOOPaste(CLASS_NAME, _constructor))(void);
-#define OOODeclare0OOOPublic(CLASS_NAME, ARGS...) \
-	OOODeclareStruct(CLASS_NAME) \
-	OOODeclareConstructorType0(CLASS_NAME, ARGS) \
-	extern OOOPaste(CLASS_NAME, _constructor) OOOPaste(CLASS_NAME, _construct);
-#define OOODeclare1OOOPublic(CLASS_NAME, ARGS...) \
-	OOODeclareStruct(CLASS_NAME) \
-	OOODeclareConstructorType1(CLASS_NAME) \
-	extern OOOPaste(CLASS_NAME, _constructor) OOOPaste(CLASS_NAME, _construct);
-#define OOODeclare0OOOPrivate(CLASS_NAME, ARGS...) \
-	OOODeclareStruct(CLASS_NAME) \
-	OOODeclareConstructorType0(CLASS_NAME, ARGS) \
-	static CLASS_NAME * OOOPaste(_, CLASS_NAME, _construct(ARGS)) GCCO_SAFE_DS; \
-	static OOOPaste(CLASS_NAME, _constructor) OOOPaste(CLASS_NAME, _construct) = OOOPaste(_, CLASS_NAME, _construct);
-#define OOODeclare1OOOPrivate(CLASS_NAME, ARGS...) \
-	OOODeclareStruct(CLASS_NAME) \
-	OOODeclareConstructorType1(CLASS_NAME) \
-	static CLASS_NAME * OOOPaste(_, CLASS_NAME, _construct)(void) GCCO_SAFE_DS; \
-	static OOOPaste(CLASS_NAME, _constructor) OOOPaste(CLASS_NAME, _construct) = OOOPaste(_, CLASS_NAME, _construct);
-#define __OOODeclare(SCOPE, CLASS_NAME, ARGS...) OOOPaste(OOODeclare, OOOIsEmpty(ARGS), SCOPE)(CLASS_NAME, ARGS)
-#define _OOODeclare(CLASS_NAME, ARGS...) __OOODeclare(OOOPublic, CLASS_NAME, ARGS)
-#define OOODeclare(ARGS...) _OOODeclare(OOOClass, ARGS)
-#define OOODeclarePrivate(ARGS...) __OOODeclare(OOOPrivate, OOOClass, ARGS)
+#define OOOConstructor2Label	1
+#define OOOImplement2Label		1
+#define OOOExport2Label			1
 
-/*
- * Export Interfaces
- */
+#define OOOConstructor2(ARGS...)	OOOList(OOOConstructor2Label,ARGS)
+#define OOOImplement2(ARGS...)	OOOList(OOOImplement2Label,ARGS)
+#define OOOExport2(ARGS...)	OOOList(OOOExport2Label,ARGS)
 
-/* Start the interfaces structure */
-#define OOOImplements \
-	typedef struct \
-	{
+#define OOOClassDeclare2ConstructorType0(NAME,ARGS...) \
+	typedef NAME * (* OOOPaste(NAME,_constructor))(ARGS);
+#define OOOClassDeclare2ConstructorType1(NAME,ARGS...) \
+	typedef NAME * (* OOOPaste(NAME,_constructor))(void);
+#define OOOClassDeclare2ConstructorType(NAME,ARGS...) OOOPaste(OOOClassDeclare2ConstructorType,OOOIsEmpty(ARGS))(NAME,ARGS)
 
-/* Add an interface */
-#define OOOImplement(INTERFACE_NAME) \
-		INTERFACE_NAME OOOPaste(t, INTERFACE_NAME);
+// Private class stuff
+//	static CLASS_NAME * OOOPaste(_, CLASS_NAME, _construct(ARGS)) GCCO_SAFE_DS; \
+//	static OOOPaste(CLASS_NAME, _constructor) OOOPaste(CLASS_NAME, _construct) = OOOPaste(_, CLASS_NAME, _construct);
+//	static CLASS_NAME * OOOPaste(_, CLASS_NAME, _construct)(void) GCCO_SAFE_DS; \
+//	static OOOPaste(CLASS_NAME, _constructor) OOOPaste(CLASS_NAME, _construct) = OOOPaste(_, CLASS_NAME, _construct);
 
-/* End the interfaces structure and declare the type name */
-#define _OOOImplementsEnd(CLASS_NAME) \
-	} \
-	OOOPaste(CLASS_NAME, _Interfaces);
-#define OOOImplementsEnd _OOOImplementsEnd(OOOClass)
-
-/*
- * Export methods
- */
-
-/* start the vtable structure */
-#define _OOOExports(CLASS_NAME) \
+#define OOOClassDeclare2(NAME,ARGS...) \
+	typedef struct OOOPaste(_,NAME) NAME; \
+	OOOClassDeclare2ConstructorType(NAME,OOOPre(OOOFilter(OOOConstructor2Label,ARGS))) \
+	extern OOOPaste(NAME,_constructor) OOOPaste(NAME,_construct); \
 	typedef struct \
 	{ \
-		void (* destroy)(CLASS_NAME * OOOThis);
-#define OOOExports _OOOExports(OOOClass)
-
-/* add an exported method */
-#define OOOExport0(CLASS_NAME, RETURN_TYPE, METHOD_NAME, ARGS...) \
-	RETURN_TYPE (* METHOD_NAME)(CLASS_NAME * OOOThis, ARGS);
-#define OOOExport1(CLASS_NAME, RETURN_TYPE, METHOD_NAME, ARGS...) \
-	RETURN_TYPE (* METHOD_NAME)(CLASS_NAME * OOOThis);
-#define _OOOExport(CLASS_NAME, RETURN_TYPE, METHOD_NAME, ARGS...) OOOPaste(OOOExport, OOOIsEmpty(ARGS))(CLASS_NAME, RETURN_TYPE, METHOD_NAME, ARGS)
-#define OOOExport(RETURN_TYPE, METHOD_NAME, ARGS...) _OOOExport(OOOClass, RETURN_TYPE, METHOD_NAME, ARGS)
-
-/* end the vtable structure and declare the type name */
-#define _OOOExportsEnd(CLASS_NAME) \
+		NAME OOOPaste(t,NAME); \
 	} \
-	OOOPaste(CLASS_NAME, _VTable);
-#define OOOExportsEnd _OOOExportsEnd(OOOClass)
-
-/*
- * End the class declaration and define the public structure
- */
-
-#define _OOODeclareEnd(CLASS_NAME) \
-	struct OOOPaste(_, CLASS_NAME) \
+	OOOPaste(NAME,_Interfaces); \
+	typedef struct \
 	{ \
-		OOOPaste(CLASS_NAME, _VTable) * pVTable; \
-		OOOPaste(CLASS_NAME, _Interfaces) tInterfaces; \
+		void (* destroy)(NAME * OOOThis); \
+		RETURN (* NAME)(CLASS_NAME * OOOThis, ARGS); \
+		RETURN (* NAME)(CLASS_NAME * OOOThis); \
+	} \
+	OOOPaste(NAME,_VTable); \
+	struct OOOPaste(_,NAME) \
+	{ \
+		OOOPaste(NAME,_VTable) * pVTable; \
+		OOOPaste(NAME,_Interfaces) tInterfaces; \
 	};
-#define OOODeclareEnd _OOODeclareEnd(OOOClass)
 
-/* forward declare a function to be used in private calls */
-#define _OOOPCDeclare0(CLASS_NAME, RETURN_TYPE, METHOD_NAME, ARGS...) \
-	static RETURN_TYPE OOOPaste(CLASS_NAME, _, METHOD_NAME)(CLASS_NAME * OOOThis, ARGS) GCCO_SAFE_DS;
-#define _OOOPCDeclare1(CLASS_NAME, RETURN_TYPE, METHOD_NAME, ARGS...) \
-	static RETURN_TYPE OOOPaste(CLASS_NAME, _, METHOD_NAME)(CLASS_NAME * OOOThis) GCCO_SAFE_DS;
-#define _OOOPCDeclare(CLASS_NAME, RETURN_TYPE, METHOD_NAME, ARGS...) OOOPaste(_OOOPCDeclare, OOOIsEmpty(ARGS))(CLASS_NAME, RETURN_TYPE, METHOD_NAME, ARGS)
-#define OOOPCDeclare(RETURN_TYPE, METHOD_NAME, ARGS...) _OOOPCDeclare(OOOClass, RETURN_TYPE, METHOD_NAME, ARGS)
-
+// Private method forward declarations (don't think we'll need these as all
+// methods can be forward declared by default using this style)
+//	static RETURN_TYPE OOOPaste(CLASS_NAME, _, METHOD_NAME)(CLASS_NAME * OOOThis, ARGS) GCCO_SAFE_DS;
+//	static RETURN_TYPE OOOPaste(CLASS_NAME, _, METHOD_NAME)(CLASS_NAME * OOOThis) GCCO_SAFE_DS;
 
 /*
  * Test interface
